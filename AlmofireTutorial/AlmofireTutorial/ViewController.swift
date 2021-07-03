c8a768190f1468bc27f93f12787921608ea2f356
Israel Torres Alvarado <israel.torres0@walmart.com>
2021-07-02 20:33:24 -0500

    Alamofire part 2

--- AlmofireTutorial/AlmofireTutorial/ViewController.swift
+++ AlmofireTutorial/AlmofireTutorial/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        AF.request("").response { response in
+            
+            
+            
+        }
+        
+        
     }
-
 
 }
 
