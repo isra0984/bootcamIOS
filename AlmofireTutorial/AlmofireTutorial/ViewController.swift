e056099512d8d63381b724a1fa080a2e7569d21d
Israel Torres Alvarado <israel.torres0@walmart.com>
2021-07-02 19:44:04 -0500

    alamofire

---
+++ AlmofireTutorial/AlmofireTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlmofireTutorial
+//
+//  Created by Israel Torres Alvarado on 02/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AlmofireTutorial/Pods/Target Support Files/Pods-AlmofireTutorial/Pods-AlmofireTutorial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlmofireTutorial : NSObject
+@end
+@implementation PodsDummy_Pods_AlmofireTutorial
+@end
